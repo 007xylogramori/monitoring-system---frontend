715c8896b02d4963b5b8388acb9e45901798d3f6
Himanshu <xylogramori@gmail.com>
2023-12-25 23:04:28 +0530

    #0 Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.css";
+import { Route, Routes } from "react-router-dom";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import Dashboard from "./Pages/Dashboard";
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route element={<Login />} path="/login" />
+        <Route element={<Signup />} path="/signup" />
+        <Route element={<Dashboard/>} path="/dashboard" />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
